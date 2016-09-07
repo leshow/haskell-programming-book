42ca7e65d385229646e61979d35d3461d9285619
Evan Cameron <evan@vectorface.ca>
2016-09-07 15:34:23 -0400

    Eq instances

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,7 @@ fstString x = x ++ " in the rain"
 sndString :: String -> String
 sndString x = x ++ " over the rainbow"
 
+sing :: String
 sing = if length x > length y then fstString x else sndString y
     where   x = "Singing"
             y = "Somewhere"
@@ -44,10 +45,7 @@ xform (a, b) = (xz a, yz b)
 munge :: (x -> y) -> (y -> (w, z)) -> x -> w
 munge a b x = fst $ b (a x)
 
-ex1 = max (length [1,2,3]) (length [8,9,10,11,12])
-ex2 = compare (3*4) (3*5)
 --ex3 = compare "Julie" True -- compare :: Ord a => a -> a -> Ordering -- here a is not the same type. won't compile.
-ex4 = (5+3) > (3+6)
 
 data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun
 data Date = Date DayOfWeek Int
@@ -66,4 +64,55 @@ instance Eq Date where
         (==) (Date weekday dayOfMonth)
             (Date weekday' dayOfMonth') =
                     weekday == weekday' && dayOfMonth == dayOfMonth'
+
+--ch6
+--
+data TisAnInteger = TisAn Integer
+
+instance Eq TisAnInteger where
+        (==) (TisAn a) (TisAn b) = a == b
+
+data TwoIntegers = Two Integer Integer
+
+instance Eq TwoIntegers where
+        (==) (Two a b) (Two x y) = a == b && x == y
+
+data StringOrInt =
+        TisAnInt Int
+          | TisAString String
+instance Eq StringOrInt where
+        (==) (TisAnInt a) (TisAnInt b) = a == b
+        (==) (TisAString a) (TisAString b) = a == b
+        (==) _ _ = False
+
+data Pair a =
+        Pair a a
+
+instance Eq a => Eq (Pair a) where
+        (==) (Pair x y) (Pair m n) = x == m && y == n
+
+data Tuple a b =
+        Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+        (==) (Tuple a b) (Tuple a' b') = a == a' && b == b'
+
+data Which a =
+        ThisOne a
+          | ThatOne a
+
+instance Eq a => Eq (Which a) where
+        (==) (ThisOne a) (ThisOne b) = a == b
+        (==) (ThatOne a) (ThatOne b) = a == b
+        (==) _ _ = False
+
+data EitherOr a b =
+        Hello a
+          | GoodBye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+        (==) (Hello a) (Hello b) = a == b
+        (==) (GoodBye a) (GoodBye b) = a == b
+        (==) _ _ = False
+
 
