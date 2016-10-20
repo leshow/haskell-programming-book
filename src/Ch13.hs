5fc1025d9e13d12fcd2cfa8ed079f72791a22604
Evan Cameron <evan@vectorface.ca>
2016-10-20 11:52:10 -0400

    module naming

--- src/Ch13.hs
+++ src/Ch13.hs
@@ -1,1 +1,12 @@
 module Ch13 where
+
+{-
+    Import questions:
+    1. Control.Monad imports forever, when in the example
+    2. Data.Bits/Database.Blacktip.Types is imported in entirety, unqualified/
+    3. the definition of the libraries types
+    4.
+        a) Control.Concurrent.MVar, Filesystem.Path.CurrentOS, Control.Concurrent
+        b) Filesystem
+        c) Control.Monad
+-}
