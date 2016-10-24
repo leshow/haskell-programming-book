f88b3b8057a3113154b1f5f40866e42226af0825
Evan Cameron <evan@vectorface.ca>
2016-10-24 16:11:52 -0400

    start hangman

--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -1,5 +1,71 @@
 module Main where
 
+import           Control.Monad (forever)
+import           Data.Char     (toLower)
+import           Data.List     (intersperse)
+import           Data.Maybe    (isJust)
+import           Data.Monoid   ((<>))
+import           System.Exit   (exitSuccess)
+import           System.Random (randomRIO)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+    dict <- readFile "data/words.txt"
+    return (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+    all_words <- allWords
+    return $ filter gamelength all_words
+    where
+        gamelength w = let
+            l = length w
+            in l > minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+    r <- randomRIO (0, length wl)
+    return $ wl !! r
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+instance Show Puzzle where
+    show (Puzzle _ discovered guessed) =
+        intersperse ' ' (renderPuzzleChar <$> discovered) <> " Guessed so far: " <> guessed
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle wrd = Puzzle wrd (map (const Nothing) wrd) []
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle wrd _ _) c = c `elem` wrd
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guessed) c = c `elem` guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing  = '_'
+renderPuzzleChar (Just c) = c
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle wrd filled s) c =
+    Puzzle wrd newFilled (c : s)
+    where
+        zipper guessed wordChar guessChar =
+            if wordChar == guessed
+            then Just wordChar
+            else guessChar
+        newFilled = zipWith (zipper c) wrd filled
