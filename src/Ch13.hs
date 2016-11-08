ed155a888c1045a308e9903f08b15ee94cfc3958
Evan Cameron <evan@vectorface.ca>
2016-11-08 12:27:15 -0500

    finish ch13

--- src/Ch13.hs
+++ src/Ch13.hs
@@ -10,3 +10,52 @@ module Ch13 where
         b) Filesystem
         c) Control.Monad
 -}
+
+import           Control.Monad
+import           Data.Char     (isAlpha, isSpace, toLower)
+import           Data.Monoid   ((<>))
+import           System.Exit
+
+palindrome :: IO ()
+palindrome = forever $ do
+    line1 <- getLine
+    let comp = format line1
+    case (comp == reverse comp) of
+        True  -> putStrLn "palindrome!"
+        False -> do
+            putStrLn "Not palindrome!"
+            exitSuccess
+
+format :: String -> String
+format = foldr (\a xs -> if (isSpace a || not (isAlpha a)) then xs else (toLower a) : xs) ""
+
+type Name = String
+type Age = Integer
+
+data Person = Person Name Age deriving Show
+
+data PersonInvalid
+    = NameEmpty
+    | AgeTooLow
+    | PersonInvalidUnknown String
+    deriving (Eq, Show)
+
+mkPerson :: Name -> Age -> Either PersonInvalid Person
+mkPerson name age
+    | name /= "" && age > 0 = Right $ Person name age
+    | name == "" = Left NameEmpty
+    | not (age > 0) = Left AgeTooLow
+    | otherwise = Left . PersonInvalidUnknown $
+                "Name was: " ++ show name ++
+                "Age was: " ++ show age
+
+gimmePerson :: IO ()
+gimmePerson = do
+    putStr "Name: "
+    name <- getLine
+    putStr "Age: "
+    age <- getLine
+    let person = mkPerson name (read age)
+    case person of
+        Left err     -> putStr "Error! " <> print err
+        Right person -> print person
