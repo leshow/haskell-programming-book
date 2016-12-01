8646dbcfba04b0bf87bffb02323faa7099de3ecb
Evan Cameron <cameron.evan@gmail.com>
2016-11-30 22:24:55 -0500

    mconcat exercise

--- src/Ch15.hs
+++ src/Ch15.hs
@@ -14,3 +14,23 @@ instance Monoid a => Monoid (Optional a) where
     mappend Nada a            = a
     mappend a Nada            = a
     mappend (Only a) (Only b) = Only (a <> b)
+
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbin' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbin' e adv noun adj =
+    e <> "! he said " <>
+    adv <> " as he jumped into his car " <>
+    noun <> " and drove off with his " <>
+    adj <> " wife."
+
+madlibbinBetter' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter' e adv noun adj =
+    mconcat [e, "! he said",
+            adv, " as he jumped into his car",
+            noun, " and he drove off with his ",
+            adj, " wife."]
