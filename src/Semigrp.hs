b8a0fef9c0ae29a49e27ec9061af5ed3e3734aec
Evan Cameron <evan@vectorface.ca>
2016-12-02 12:19:37 -0500

    finish semigroup/monoid ex

--- src/Semigrp.hs
+++ src/Semigrp.hs
@@ -2,7 +2,7 @@ module Semigrp where
 
 import           Data.List.NonEmpty as N
 import           Data.Semigroup
-import           Test.QuickCheck
+import           Test.QuickCheck    hiding (Failure, Success)
 -- Semigroup is Monoid minus the identity laws.
 -- class Semigroup a where
 -- (<>) :: a -> a -> a  -- it is in GHC 8 and up
@@ -18,7 +18,11 @@ zs = xs <> ys
 data Trivial = Trivial deriving (Eq, Show)
 
 instance Semigroup Trivial where
-    a <> b = Trivial
+    _ <> _ = Trivial
+
+instance Monoid Trivial where
+    mempty = Trivial
+    mappend = (<>)
 
 instance Arbitrary Trivial where
     arbitrary = return Trivial
@@ -30,6 +34,10 @@ newtype Identity a = Identity a deriving (Eq, Show)
 
 instance Semigroup a => Semigroup (Identity a)  where
     Identity a <> Identity b = Identity (a <> b)
+
+instance Monoid a => Monoid (Identity a) where
+    mempty = Identity mempty
+    mappend (Identity a) (Identity b) = Identity (a `mappend` b)
 
 instance Arbitrary a => Arbitrary (Identity a) where
     arbitrary = do
@@ -44,6 +52,10 @@ data Two a b = Two a b deriving (Eq, Show)
 
 instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
     (Two a b) <> (Two x y) = Two (a <> x) (b <> y)
+
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+    mempty = Two mempty mempty
+    mappend (Two a b) (Two x y) = Two (a `mappend` x) (b `mappend` y)
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
     arbitrary = do
@@ -100,6 +112,10 @@ instance Semigroup BoolConj where
     BoolConj True <> BoolConj True = BoolConj True
     BoolConj _ <> BoolConj _ = BoolConj False
 
+instance Monoid BoolConj where
+    mempty = BoolConj True
+    mappend = (<>)
+
 instance Arbitrary BoolConj where
     arbitrary = elements [BoolConj False, BoolConj True]
 
@@ -112,6 +128,10 @@ newtype BoolDisj = BoolDisj Bool deriving (Eq, Show)
 instance Semigroup BoolDisj where
     BoolDisj False <> BoolDisj False = BoolDisj False
     BoolDisj _ <> BoolDisj _ = BoolDisj True
+
+instance Monoid BoolDisj where
+    mempty = BoolDisj False
+    mappend = (<>)
 
 instance Arbitrary BoolDisj where
     arbitrary = elements [BoolDisj False, BoolDisj True]
@@ -149,10 +169,107 @@ newtype Combine a b = Combine { unCombine :: a -> b }
 instance (Semigroup a, Semigroup b) => Semigroup (Combine a b) where
     Combine f <> Combine g = Combine $ \m -> f m <> g m
 
+instance (Monoid a, Monoid b) => Monoid (Combine a b) where
+    mempty = Combine $ \m -> mempty
+    mappend (Combine f) (Combine g) = Combine $ \m -> f m `mappend` g m
+
+-- 10
+
+newtype Comp a = Comp { unComp :: a -> a }
+
+instance Semigroup a => Semigroup (Comp a) where
+    Comp a <> Comp b = Comp $ \m -> a m <> b m
+
+instance Monoid a => Monoid (Comp a) where
+    mempty = Comp id
+    mappend (Comp f) (Comp g) = Comp $ f . g
+
+-- 11
+
+data Validation a b
+    = Failure a
+    | Success b
+    deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Validation a b) where
+    Success a <> Success b = Success a
+    Failure a <> Success b = Success b
+    Success a <> Failure b = Success a
+    Failure a <> Failure b = Failure (a <> b) -- this is allowed since 'a' is a semigroup instance
+    -- and Failure values are always 'a'
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Failure a, Success b]
+
+type ValidAssoc = Validation String String
+                -> Validation String String
+                -> Validation String String
+                -> Bool
+
+-- 12
+
+newtype AccumulateRight a b = AccumulateRight (Validation a b) deriving (Eq, Show)
+
+instance Semigroup b => Semigroup (AccumulateRight a b) where
+    AccumulateRight (Success a) <> AccumulateRight (Success b) = AccumulateRight (Success (a <> b))
+    AccumulateRight (Success a) <> AccumulateRight (Failure b) = AccumulateRight (Success a)
+    AccumulateRight (Failure a) <> AccumulateRight (Success b) = AccumulateRight (Success b)
+    AccumulateRight (Failure a) <> AccumulateRight (Failure b) = AccumulateRight (Failure a)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AccumulateRight a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [AccumulateRight (Failure a), AccumulateRight (Success b)]
+
+type AccRAssoc = AccumulateRight String String
+                -> AccumulateRight String String
+                -> AccumulateRight String String
+                -> Bool
+
+-- 13
+
+newtype AccumulateBoth a b = AccumulateBoth (Validation a b) deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (AccumulateBoth a b) where
+    AccumulateBoth a <> AccumulateBoth b = AccumulateBoth (a <> b)
+    -- if we know a and b are Semigroup then we can destructure and delegate to their instances
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AccumulateBoth a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [AccumulateBoth (Failure a), AccumulateBoth (Success b)]
+
+type AccBAssoc = AccumulateBoth String String
+                -> AccumulateBoth String String
+                -> AccumulateBoth String String
+                -> Bool
+
+
+-- Monoid 8
+
+newtype Mem s a = Mem { runMem :: s -> (a,s) }
+
+instance Monoid a => Monoid (Mem s a) where
+    mempty = Mem $ \s -> (mempty, s)
+    mappend (Mem f) (Mem g) = Mem $ \s -> (fst (f s) `mappend` fst (g s), snd (g (snd $ f s)))
 
 -- tests
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidAssoc :: (Semigroup m, Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Semigroup m, Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Semigroup m, Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
 
 runSemiTests = do
     quickCheck (semigroupAssoc :: TrivialAssoc)
@@ -163,3 +280,23 @@ runSemiTests = do
     quickCheck (semigroupAssoc :: BoolConjAssoc)
     quickCheck (semigroupAssoc :: BoolDisjAssoc)
     quickCheck (semigroupAssoc :: OrAssoc)
+    quickCheck (semigroupAssoc :: ValidAssoc)
+    quickCheck (semigroupAssoc :: AccRAssoc)
+    quickCheck (semigroupAssoc :: AccBAssoc)
+
+runMonoidTests = do
+    quickCheck (semigroupAssoc :: TrivialAssoc)
+    quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+    quickCheck (monoidRightIdentity :: Trivial -> Bool)
+    quickCheck (semigroupAssoc :: IdAssoc)
+    quickCheck (monoidLeftIdentity :: Identity String -> Bool)
+    quickCheck (monoidRightIdentity :: Identity String -> Bool)
+    quickCheck (semigroupAssoc :: TwoAssoc)
+    quickCheck (monoidLeftIdentity :: Two String String -> Bool)
+    quickCheck (monoidRightIdentity :: Two String String -> Bool)
+    quickCheck (semigroupAssoc :: BoolConjAssoc)
+    quickCheck (monoidLeftIdentity :: BoolConj -> Bool)
+    quickCheck (monoidRightIdentity :: BoolConj -> Bool)
+    quickCheck (semigroupAssoc :: BoolDisjAssoc)
+    quickCheck (monoidLeftIdentity :: BoolDisj -> Bool)
+    quickCheck (monoidRightIdentity :: BoolDisj -> Bool)
