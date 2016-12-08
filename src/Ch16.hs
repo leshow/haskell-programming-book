b302ef4f059e86f0414c2739924400dca7a5a0a5
Evan Cameron <evan@vectorface.ca>
2016-12-08 14:17:15 -0500

    functor instances

--- src/Ch16.hs
+++ src/Ch16.hs
@@ -136,3 +136,19 @@ e = let     ioi = readIO "1" :: IO Integer
             changed = fmap (read . ("123"++) . show) ioi
 
     in      fmap (*3) changed
+
+
+data Two a b
+    = Two a b deriving (Eq, Show)
+
+data Or a b
+    = First a
+    | Second b
+    deriving (Eq, Show)
+
+instance Functor (Two a) where
+    fmap f (Two a b) = Two a (f b)
+
+instance Functor (Or a) where
+    fmap _ (First a)  = First a
+    fmap f (Second b) = Second (f b)
