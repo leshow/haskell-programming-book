2ae1135dc6cc0a247592150a58c2ba2e151ff831
Evan Cameron <evan@vectorface.ca>
2016-12-08 13:32:49 -0500

    practice functor lifting

--- src/Ch16.hs
+++ src/Ch16.hs
@@ -77,3 +77,62 @@ so,
     ~ b -> (b -> c) -> c
 
 -}
+
+replaceWithP :: b -> Char
+replaceWithP = const 'p'
+
+lms :: [Maybe [Char]]
+lms = [Just "ave", Nothing, Just "woohoo"]
+
+replaceWithP' :: [Maybe [Char]] -> Char
+replaceWithP' = replaceWithP
+
+liftedReplace :: Functor f => f a -> f Char
+liftedReplace = fmap replaceWithP
+
+liftedReplace' :: [Maybe [Char]] -> [Char]
+liftedReplace' = liftedReplace
+
+twiceLifted :: (Functor f1, Functor f) => f (f1 a) -> f (f1 Char)
+twiceLifted = (fmap . fmap) replaceWithP
+
+twiceLifted' :: [Maybe [Char]] -> [Maybe Char]
+twiceLifted' = twiceLifted
+
+thriceLifted :: (Functor f2, Functor f1, Functor f) => f (f1 (f2 a)) -> f (f1 (f2 Char))
+thriceLifted = (fmap . fmap . fmap) replaceWithP
+
+thriceLifted' :: [Maybe [Char]] -> [Maybe [Char]]
+thriceLifted' = thriceLifted
+
+liftStuff :: IO ()
+liftStuff = do
+    putStr "replaceWithP' lms: "
+    print (replaceWithP' lms)
+    putStr "liftedReplace lms: "
+    print (liftedReplace lms)
+    putStr "liftedReplace' lms: "
+    print (liftedReplace' lms)
+    putStr "twiceLifted lms: "
+    print (twiceLifted lms)
+    putStr "twiceLifted' lms: "
+    print (twiceLifted' lms)
+    putStr "thriceLifted lms: "
+    print (thriceLifted lms)
+    putStr "thriceLifted' lms: "
+    print (thriceLifted' lms)
+
+-- 1
+a = (+1) <$> read "[1]" :: [Int]
+-- 2
+b = (fmap . fmap) (++ "lol") (Just ["Hi,", "Hello"])
+-- 3
+c = fmap (*2) (*2)
+-- 4
+d = fmap ((return '1' ++) . show) (\x -> [x, 1..3])
+-- 5
+e :: IO Integer
+e = let     ioi = readIO "1" :: IO Integer
+            changed = fmap (read . ("123"++) . show) ioi
+
+    in      fmap (*3) changed
