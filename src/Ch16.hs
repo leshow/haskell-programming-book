0f3d41699e5e5c817343024ee65da1ac60bd2619
Evan Cameron <evan@vectorface.ca>
2016-12-16 15:20:33 -0500

    functor exercises

--- src/Ch16.hs
+++ src/Ch16.hs
@@ -416,3 +416,8 @@ data TalkToMe a
     = Halt
     | Print String a
     | Read (String -> a)
+
+instance Functor TalkToMe where
+    fmap _ Halt        = Halt
+    fmap f (Print s a) = Print s (f a)
+    fmap f (Read g)    = Read (f . g)
