754fa54d38366c64b9eeaa46b5696fdd983181b4
Evan Cameron <evan@vectorface.ca>
2017-01-04 17:16:19 -0500

    applicative exercises

--- src/Ch17.hs
+++ src/Ch17.hs
@@ -38,15 +38,32 @@ z = lookup 2 $ zip [1,2,3] [4,5,6]
 
 tupled :: Maybe (Integer, Integer)
 tupled = liftA2 (,) y z
+-- (,) <$> y <*> z
 
-x :: Maybe Int
-x = elemIndex 3 [1..5]
+x' :: Maybe Int
+x' = elemIndex 3 [1..5]
 
-yx :: Maybe Int
-yx = elemIndex 4 [1..5]
+y' :: Maybe Int
+y' = elemIndex 4 [1..5]
 
 max' :: Int -> Int -> Int
 max' = max
 
--- maxed :: Maybe Int
--- maxed = max' x y
+maxed :: Maybe Int
+maxed = max' <$> x' <*> y'
+-- :t max' <$> x'
+-- Maybe (Int -> Int)  aka. f (a -> b) -- the first arg to applicative's <*>
+-- so we can take that and use <*> y' to pass in f a
+
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+x'' :: Maybe Integer
+x'' = lookup 2 $ zip xs ys
+
+y'' :: Maybe Integer
+y'' = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = sum <$> ((,) <$> x'' <*> y'')
+-- sum <$> liftA2 (,) x'' y''
