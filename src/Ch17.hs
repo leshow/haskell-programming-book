4c41395bdc01c540439db814307f48f0668e9763
Evan Cameron <evan@vectorface.ca>
2017-01-06 17:00:04 -0500

    moomoo

--- src/Ch17.hs
+++ src/Ch17.hs
@@ -154,4 +154,18 @@ data Cow
     name     :: String
     , age    :: Int
     , weight :: Int
-} deriving (Eq, Show)
+    } deriving (Eq, Show)
+
+noEmpty :: String -> Maybe String
+noEmpty ""  = Nothing
+noEmpty str = Just str
+
+noNegative :: Int -> Maybe Int
+noNegative n    | n >= 0 = Just n
+                | otherwise = Nothing
+
+cowFromString :: String -> Int -> Int -> Maybe Cow
+cowFromString n a w =
+    Cow <$> noEmpty n
+    <*> noNegative a
+    <*> noNegative w
