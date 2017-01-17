11a4bc8a874c356743ace3f37c174c3c048bd681
Evan Cameron <evan@vectorface.ca>
2017-01-17 15:03:20 -0500

    my first Monad instance

--- src/Ch18.hs
+++ src/Ch18.hs
@@ -2,8 +2,11 @@
 -- Monads, not burritos
 module Ch18 where
 
-import           Control.Monad               (join)
+import           Control.Monad               (join, liftM, liftM2)
 import           Control.Parallel.Strategies
+import           Test.QuickCheck
+import           Test.QuickCheck.Checkers
+import           Test.QuickCheck.Classes
 
 -- is related to fmap and applicative
 -- this holds:
@@ -96,3 +99,166 @@ instance Monad (Sum a) where
     return = pure
     (>>=) (First a) _  = First a
     (>>=) (Second b) f = f b
+
+-- right identity
+-- m >>= return = m
+-- left identity
+-- return x >>= f = f x
+
+-- (>=>) :: Monad m => (a -> m b) -> (b -> m c) -> a -> m c
+-- flip (.) :: (a -> b) -> (b -> c) -> a -> c
+-- example.
+-- sayHi :: String -> IO String
+-- readM :: Read a => String -> IO a
+-- sayHi >=> readM
+--
+-- (String -> IO String) -> (String -> IO a) -> String -> IO a
+
+-- Chapter Exercises
+
+-- 1
+data Nope a = NopeDotJpg deriving (Eq, Show)
+
+instance Functor Nope where
+    fmap _ _ = NopeDotJpg
+
+instance Applicative Nope where
+    pure _ = NopeDotJpg
+    (<*>) _ _ = NopeDotJpg
+
+instance Monad Nope where
+    return _ = NopeDotJpg
+    (>>=) _ _ = NopeDotJpg
+
+instance Arbitrary (Nope a) where
+    arbitrary = return NopeDotJpg
+
+instance Eq a => EqProp (Nope a) where (=-=) = eq
+
+-- 2
+
+data PhbtEither b a = Left a | Right b deriving (Eq, Show)
+
+instance Functor (PhbtEither b) where
+    fmap f (Ch18.Left a)  = Ch18.Left $ f a
+    fmap _ (Ch18.Right b) = Ch18.Right b
+
+instance Applicative (PhbtEither b) where
+    pure = Ch18.Left
+
+    (<*>) (Ch18.Right a) _ = Ch18.Right a
+    (<*>) (Ch18.Left f) b  = fmap f b
+
+instance Monad (PhbtEither b) where
+    return = pure
+
+    (>>=) (Ch18.Right a) _ = Ch18.Right a
+    (>>=) (Ch18.Left a) f  = f a
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (PhbtEither b a) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Ch18.Left a, Ch18.Right b]
+
+instance (Eq a, Eq b) => EqProp (PhbtEither b a) where (=-=) = eq
+
+-- 3
+
+
+newtype Identity a
+    = Identity a
+    deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+    fmap f (Identity a) = Identity $ f a
+
+instance Applicative Identity where
+    pure = Identity
+    (<*>) (Identity f) = fmap f
+
+instance Monad Identity where
+    return = pure
+    (>>=) (Identity a) f = f a
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = do
+        a <- arbitrary
+        return $ Identity a
+
+instance Eq a => EqProp (Identity a) where (=-=) = eq
+
+-- 4
+data List a
+    = Nil
+    | Cons a (List a)
+    deriving (Eq, Show)
+
+instance Functor List where
+    fmap _ Nil         = Nil
+    fmap f (Cons a as) = Cons (f a) (fmap f as)
+
+instance Monoid (List a) where
+    mempty = Nil
+    mappend Nil lb         = lb
+    mappend (Cons a la) lb = Cons a (la `mappend` lb)
+
+instance Applicative List where
+    pure a = Cons a Nil
+
+    (<*>) Nil Nil        = Nil
+    (<*>) Nil _          = Nil
+    (<*>) _ Nil          = Nil
+    (<*>) (Cons f fs) as = mappend (fmap f as) (fs <*> as)
+
+instance Monad List where
+    return = pure
+    as >>= f = join $ fmap f as
+
+instance Arbitrary a => Arbitrary (List a) where
+    arbitrary = genList
+
+genList :: Arbitrary a => Gen (List a)
+genList = do
+    h <- arbitrary
+    t <- genList
+    frequency [(3, return $ Cons h t),
+             (1, return Nil)]
+
+instance Eq a => EqProp (List a) where (=-=) = eq
+
+
+type I = Int
+
+runMonad :: IO ()
+runMonad = do
+  putStr "\n-- Nope"
+  quickBatch $ monad (undefined :: Nope (I, I, I))
+  putStr "\n-- PEither"
+  quickBatch $ monad (undefined :: PhbtEither I (I, I, I))
+  putStr "\n-- Identity"
+  quickBatch $ monad (undefined :: Identity (I, I, I))
+  putStr "\n-- List"
+  quickBatch $ monad (undefined :: List (I, I, I))
+
+j :: Monad m => m (m a) -> m a
+j = join
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = liftM -- remember liftM/liftA is fmap, just with different constraints.
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 = liftM2
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip (<*>)
+
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _ = return []
+meh (a:as) f = do
+    a' <- f a
+    as' <- meh as f
+    return (a':as')
+
+flipType :: (Monad m) => [m a] -> m [a]
+flipType = flip meh id
