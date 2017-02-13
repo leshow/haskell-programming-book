7586ad30809b936fb9324c9475ae451b7ae2582b
Evan Cameron <cameron.evan@gmail.com>
2017-02-12 22:03:57 -0500

    ch20 finished.

--- src/Ch20.hs
+++ src/Ch20.hs
@@ -117,3 +117,16 @@ data Three a b c = Three a b c
 
 instance Foldable (Three a b) where
     foldMap f (Three a b c) = f c
+
+data Three' a b = Three' a b b
+
+instance Foldable (Three' a) where
+    foldMap f (Three' a b b') = f b <> f b'
+
+data Four' a b = Four' a b b b
+
+instance Foldable (Four' a) where
+    foldMap f (Four' a b b' b'') = f b <> f b' <> f b''
+
+filterF :: (Applicative f, Foldable t, Monoid (f a)) => (a -> Bool) -> t a -> f a
+filterF f = foldMap (\x -> if f x then pure x else mempty)
