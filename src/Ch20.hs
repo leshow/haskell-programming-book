2a91c3b7235a3bbe571867b011a7c09cb8e0297a
Evan Cameron <cameron.evan@gmail.com>
2017-02-12 21:47:18 -0500

    ch20 library exercises

--- src/Ch20.hs
+++ src/Ch20.hs
@@ -2,7 +2,11 @@
 
 module Ch20 where
 
+import           Data.Foldable
 import           Data.Monoid
+import           Prelude       hiding (length, maximum, minimum, null, product,
+                                sum)
+
 
 -- class Foldable (t :: * -> *) where
 --     fold :: forall m. Monoid m => t m -> m
@@ -32,3 +36,67 @@ instance Foldable Opt where
 
     foldMap _ Nada     = mempty
     foldMap f (Yasa a) = f a
+
+-- Library Exercises
+
+-- 1.
+sum :: (Foldable t, Num a) => t a -> a
+sum = getSum . foldMap Sum
+
+-- 2.
+product :: (Foldable t, Num a) => t a -> a
+product = getProduct . foldMap Product
+
+-- 3.
+elem :: (Foldable t, Eq a) => a -> t a -> Bool
+elem a = getAny . foldMap (\x -> Any $ x == a)
+
+-- 4.
+-- minimum :: (Foldable t, Ord a) => t a -> Maybe a
+-- minimum = fmap getMin . getOption . foldMap (Option . Just . Min)
+data Min a = Min { getMin :: Maybe a } deriving (Eq, Show)
+
+instance Ord a => Monoid (Min a) where
+    mempty = Min Nothing
+    mappend (Min Nothing) a = a
+    mappend a (Min Nothing) = a
+    mappend (Min a) (Min b) = Min $ min a b
+
+chmin :: (Foldable t, Ord a) => t a -> Maybe a
+chmin = getMin . foldMap (\x -> Min {getMin = Just x}) -- equivalent to Min $ Just x OR Min . Just
+
+-- 5.
+
+data Max a = Max { getMax :: Maybe a } deriving (Eq, Show)
+
+instance Ord a => Monoid (Max a) where
+    mempty = Max Nothing
+    mappend (Max Nothing) a = a
+    mappend a (Max Nothing) = a
+    mappend (Max a) (Max b) = Max $ max a b
+
+chmax :: (Foldable t, Ord a) => t a -> Maybe a
+chmax = getMax . foldMap (Max . Just)
+
+-- 6
+null :: (Foldable t) => t a -> Bool
+null = getAll . foldMap (\_ -> All False)
+
+-- 7
+length :: Foldable t => t a -> Int
+length = foldr (\_ acc -> acc + 1) 0
+
+-- 8
+toList :: Foldable t => t a -> [a]
+toList = foldr (:) []
+
+-- 9
+myfold :: (Foldable t, Monoid m) => t m -> m
+myfold = foldr (<>) mempty
+
+-- 10
+myFoldMap :: (Foldable t, Functor t, Monoid m) => (a -> m) -> t a -> m
+myFoldMap f = fold . fmap f
+
+otherFMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+otherFMap f = foldr (\x acc -> f x `mappend` acc) mempty
