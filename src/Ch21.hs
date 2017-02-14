f531a12313c4851dba40d109b8427528e361e231
Evan Cameron <cameron.evan@gmail.com>
2017-02-13 23:27:53 -0500

    start ch21

---
+++ src/Ch21.hs
@@ -0,0 +1,5 @@
+module Ch21 where
+
+-- Traversable is a superclass of Foldable that defines a type which
+-- maps each element of a structure to an action, and evaluates actions from left
+-- to right. 
