66466c9903143838b439f2e4129b802f6a2664c2
Evan Cameron <cameron.evan@gmail.com>
2017-02-16 22:14:10 -0500

    result functor/app/fold/trav

--- src/Ch21.hs
+++ src/Ch21.hs
@@ -17,3 +17,34 @@ module Ch21 where
 -- however if there's a Nothing in the list, the whole thing will return Nothing
 -- fmap sum $ sequenceA [Just 1, Just 2, Nothing]
 -- Nothing
+
+
+-- mapM is just traverse -but only for lists, a more general version
+
+-- traversable use: when you need to flip two type constructors around, or map something then flip them
+
+
+data Result a b
+    = Error a
+    | Ok b
+    deriving (Eq, Ord, Show)
+
+instance Functor (Result a) where
+    fmap _ (Error a) = Error a
+    fmap f (Ok b)    = Ok (f b)
+
+instance Applicative (Result a) where
+    pure = Ok
+    Ok f <*> r = fmap f r
+    Error f <*> l = Error f
+
+instance Foldable (Result a) where
+    foldMap f (Error e) = mempty
+    foldMap f (Ok a)    = f a
+
+    foldr f b (Error e) = b
+    foldr f b (Ok a)    = f a b
+
+instance Traversable (Result a) where
+    traverse f (Error e) = pure $ Error e
+    traverse f (Ok b)    =  Ok <$> f b
