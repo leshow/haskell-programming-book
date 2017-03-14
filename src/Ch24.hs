f9943e226432e4c35801900d7f0e7fb68e35aef9
Evan Cameron <evan@vectorface.com>
2017-03-14 17:12:41 -0400

    parserc

--- src/Ch24.hs
+++ src/Ch24.hs
@@ -4,3 +4,9 @@ module Ch24 where
 -- Learning Parsers
 
 import           Text.Trifecta
+
+stop :: Parser a
+stop = unexpected "stop"
+
+one = char '1' -- read a single char '1'
+one' = one >> stop -- read a sincle char '1' then die
