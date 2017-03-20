e94d323b40d2d3985d82d50d0145a637cc8897a5
Evan Cameron <evan@vectorface.com>
2017-03-20 16:58:44 -0400

    applicative compose

--- src/Ch25.hs
+++ src/Ch25.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Ch25 where
+
+import           Control.Applicative (liftA2)
 
 newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
 
@@ -7,3 +11,12 @@ newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
 
 instance (Functor f, Functor g) => Functor (Compose f g) where
     fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+    pure :: a -> Compose f g a
+    pure a = Compose $ (pure . pure) a
+
+    (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+    Compose f <*> Compose a = Compose $ liftA2 (<*>) f a
+
