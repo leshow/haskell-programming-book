02899f4a5a3582a64f19bc8a9d88a373a0e625d4
Evan Cameron <evan@vectorface.com>
2017-03-20 17:06:30 -0400

    bifunctor

--- src/Ch25.hs
+++ src/Ch25.hs
@@ -20,3 +20,29 @@ instance (Applicative f, Applicative g) => Applicative (Compose f g) where
     (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
     Compose f <*> Compose a = Compose $ liftA2 (<*>) f a
 
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+    foldMap f (Compose fga) = (foldMap . foldMap) f fga
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+    traverse f (Compose fga) = Compose <$> (traverse . traverse) f fga
+
+
+
+class Bifunctor p where
+    bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+    bimap f g = first f . second g
+    first :: (a -> b) -> p a c -> p b c
+    first f = bimap f id
+    second :: (b -> c) -> p a b -> p a c
+    second = bimap id
+
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+    bimap f g (Deux a b) = Deux (f a) (g b)
+
+data Const a b = Const a
+
+instance Bifunctor Const where
+    bimap f g (Const a) = Const (f a)
