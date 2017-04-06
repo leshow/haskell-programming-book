8dfe91de651b89779103cd07da1a60932c0e3302
Evan Cameron <evan@vectorface.com>
2017-04-06 16:19:53 -0400

    w/ readert

--- src/Ch26-Morra.hs
+++ src/Ch26-Morra.hs
@@ -3,11 +3,13 @@
 
 module Morra where
 
-import           Control.Monad             (forever)
+import           Control.Monad              (forever, when)
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.State
-import           Data.Monoid               ((<>))
+import           Data.Monoid                ((<>))
+import           System.Exit                (exitSuccess)
 import           System.Random
 
 data Type = Odds | Evens deriving (Eq, Show)
@@ -15,38 +17,46 @@ data Type = Odds | Evens deriving (Eq, Show)
 newRand :: IO Int
 newRand = randomRIO (0,1)
 
-type Morra = StateT Game IO
+-- partially applied ReaderT
+type Morra = ReaderT Config (StateT Game IO)
+
 data Game = Game {
-    human        :: Type
-    , humanScore :: Int
-    , pc         :: Type
-    , pcScore    :: Int
-    , rounds     :: Int
+    humanScore :: Int
+    , pcScore  :: Int
+    , rounds   :: Int
+}
+
+data Config = Config {
+    human :: Type
+    , pc  :: Type
 }
 
 oddOrEven :: Int -> Type
 oddOrEven r = if even r then Evens else Odds
 
-initial :: Type -> Type -> Game
-initial hum comp = Game hum 0 comp 0 0
+initialGame :: Game
+initialGame = Game 0 0 0
 
-main :: IO ()
-main = do
+initConfig :: Type -> Type -> Config
+initConfig hum comp = Config hum comp
+
+getConfig :: IO Config
+getConfig = do
     computer <- oddOrEven <$> newRand
     person <- oddOrEven <$> newRand
-    let game = initial person computer
-    forever $ runStateT rollDie game
+    return $ initConfig person computer
 
 humanInc :: Game -> Game
-humanInc (Game h hs p ps r) = Game h (hs+1) p ps (r+1)
+humanInc (Game hs ps r) = Game (hs+1) ps (r+1)
 
 pcInc :: Game -> Game
-pcInc (Game h hs p ps r) = Game h hs p (ps+1) (r+1)
+pcInc (Game hs ps r) = Game hs (ps+1) (r+1)
 
 
-rollDie :: StateT Game IO ()
+rollDie :: Morra ()
 rollDie = do
-    game@Game{..} <- get
+    game@Game{..} <- lift get
+    config@Config{..} <- ask
     humanRoll <- liftIO getLine
     liftIO $ putStrLn "Guess accepted."
     pcRoll <- liftIO $ randomRIO (0,1)
@@ -56,5 +66,12 @@ rollDie = do
         (newGame, title) = if winner then (humanInc game, "human") else (pcInc game, "pc")
     liftIO $ print winner
     liftIO $ putStrLn ("Winner of this round is " <> title <> "!!")
-    put newGame
+    lift $ put newGame
     return ()
+
+
+main :: IO ()
+main = do
+    config <- getConfig
+    let game = initialGame
+    forever $ runStateT (runReaderT rollDie config) game
