be49f0723ca5870044ea1179578d31cbcbc9f583
Evan Cameron <cameron.evan@gmail.com>
2017-04-14 12:56:52 -0400

    remove lints

--- src/Ch26-Morra.hs
+++ src/Ch26-Morra.hs
@@ -52,7 +52,7 @@ initialGame :: Game
 initialGame = Game 0 0 0 []
 
 initConfig :: Type -> Type -> Config
-initConfig hum comp = Config hum comp
+initConfig = Config
 
 getConfig :: IO Config
 getConfig = do
@@ -74,7 +74,7 @@ runGame = do
     case input of
         Roll num -> do
             liftIO $ putStrLn "Guess accepted."
-            pcRoll <- liftIO $ newRand
+            pcRoll <- liftIO newRand
             let total = num + pcRoll
                 typeOf = oddOrEven total
                 winner = typeOf == human
@@ -83,7 +83,7 @@ runGame = do
             liftIO $ do
                 putStrLn $ if winner then "Winner" else "Loser"
                 putStrLn ("Winner of this round is " <> title <> "!!")
-                putStrLn (show newGame)
+                print newGame
             lift $ put newGame
             runGame
         Cmd c -> do
@@ -111,8 +111,7 @@ getDig c
     | isLetter c = Cmd c
 
 helpText :: IO ()
-helpText = do
-    putStrLn "Press '1' for Odd '2' for Even, or q for quit"
+helpText = putStrLn "Press '1' for Odd '2' for Even, or q for quit"
 
 main :: IO ()
 main = do
