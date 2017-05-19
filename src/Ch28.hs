a5e172089dd49ff4fc8be28c5300d46e4d4f7bdc
Evan Cameron <evan@vectorface.com>
2017-05-19 16:27:09 -0400

    fixed

--- src/Ch28.hs
+++ src/Ch28.hs
@@ -3,13 +3,14 @@ module Main where
 import           Criterion.Main
 
 infixl 9 !?
-
-
+{-# INLINABLE (!?) #-}
 (!?) :: [a] -> Int -> Maybe a
-_       !? n | n < 0 = Nothing
-[]      !? _ = Nothing
-(x:_)   !? 0 = Just x
-(_:xs)  !? n = xs !? (n-1)
+xs !? n
+    | n < 0 = Nothing
+    | otherwise =
+        foldr (\x r k -> case k of
+            0 -> Just x
+            _ -> r (k-1)) (const Nothing) xs n
 
 myList :: [Int]
 myList = [1..9999]
