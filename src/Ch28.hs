c1a2c3361cff8543a864661085d07d3fe902cfaf
Evan Cameron <evan@vectorface.com>
2017-05-19 14:58:17 -0400

    criterion ch 28

---
+++ src/Ch28.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import           Criterion.Main
+
+infixl 9 !?
+
+
+(!?) :: [a] -> Int -> Maybe a
+_       !? n | n < 0 = Nothing
+[]      !? _ = Nothing
+(x:_)   !? 0 = Just x
+(_:xs)  !? n = xs !? (n-1)
+
+myList :: [Int]
+myList = [1..9999]
+
+main :: IO ()
+main = defaultMain
+    [ bench "index list 9999"
+    $ whnf (myList !!) 9998
+    , bench "index list maybe index 9999"
+    $ whnf (myList !?) 9998
+    ]
+
