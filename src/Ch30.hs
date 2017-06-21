67dd0b4476528ba95f3781c568a2e04472c6336b
Evan Cameron <evan@vectorface.com>
2017-06-21 16:51:13 -0400

    exception

--- src/Ch30.hs
+++ src/Ch30.hs
@@ -2,9 +2,10 @@
 {-# LANGUAGE GADTs                     #-}
 {-# LANGUAGE MultiWayIf                #-}
 
-module Ch30 where
+module Main where
 
-import           Control.Exception (ArithException (..), AsyncException (..))
+import           Control.Exception (ArithException (..), AsyncException (..),
+                                    SomeException (..), catch)
 import           Data.Typeable
 
 data MyException = forall e. (Show e, Typeable e) => MyException e
@@ -16,3 +17,13 @@ multiError :: Int -> Either MyException Int
 multiError n = if | n == 0 -> Left $ MyException DivideByZero
                   | n == 1 -> Left $ MyException StackOverflow
                   | otherwise -> Right n
+
+
+handler :: SomeException -> IO ()
+handler (SomeException e) = do
+    print (typeOf e)
+    putStrLn $ "We errored! It was: " ++ show e
+
+
+main :: IO ()
+main = writeFile "zzz" "hi" `catch` handler
