7440e502d1642d1d9b4241f486affca9ac3b1613
Evan Cameron <evan@vectorface.com>
2017-12-12 12:26:08 -0500

    bounded fib channel vs list

--- go-haskell/src/BoundedChannels.hs
+++ go-haskell/src/BoundedChannels.hs
@@ -6,8 +6,47 @@ import           Control.Concurrent.BoundedChan (BoundedChan, getChanContents,
                                                  newBoundedChan, readChan,
                                                  writeChan)
 import           Control.Monad                  (foldM_, forM_)
-import           Control.Monad                  (guard)
 import           Data.Maybe                     (catMaybes, isJust)
 
---range :: TBQueue (Maybe a) -> IO [a]
+range :: BoundedChan (Maybe a) -> IO [a]
+range chan = do
+    xs <- getChanContents chan
+    pure $ catMaybes (takeWhile isJust xs)
 
+fibonnacci :: Int -> BoundedChan (Maybe Int) -> IO ()
+fibonnacci n chan = do
+    foldM_ iter (0, 1) [0..n] -- send fibs up to n
+    writeChan chan Nothing -- write done
+    where
+        iter :: (Int, Int) -> t -> IO (Int, Int)
+        iter (x, y) _ = do
+            writeChan chan (Just x)
+            pure (y, x + y)
+
+main :: IO ()
+main = do
+    let size = 10
+    ch <- newBoundedChan size
+    async $ fibonnacci size ch
+    ch' <- range ch
+    forM_ ch' print
+    pure ()
+
+fibonacciByList :: Int -> [Int]
+fibonacciByList n = (fib !!) <$> [0..n]
+    where
+        fib = 0 : 1 : zipWith (+) fib (tail fib)
+
+fibList :: Int -> [Int]
+fibList n = fib n 0 1
+    where
+        fib :: Int -> Int -> Int -> [Int]
+        fib 0 a _ = [a]
+        fib t a b = a : fib (t-1) b (a + b)
+
+mainByList :: IO ()
+mainByList = do
+    let size = 10
+        fibs = fibonacciByList size
+    forM_ fibs print
+    pure ()
