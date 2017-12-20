eae2321be78a67ec8e96a657696a221fc63adc33
Evan Cameron <evan@vectorface.com>
2017-12-20 12:20:06 -0500

    start trees exercise

--- go-haskell/src/BinTree.hs
+++ go-haskell/src/BinTree.hs
@@ -6,16 +6,19 @@ module BinTree where
 import           Control.Concurrent       (Chan, getChanContents, newChan,
                                            readChan, writeChan)
 import           Control.Concurrent.Async (async)
-import           Control.Monad            (forever)
+import           Control.Monad            (forever, liftM3)
 import           Data.Foldable            (for_)
+import           Data.List                (sort)
 import           Data.Maybe               (catMaybes, isJust)
+import           Data.Monoid              ((<>))
 import           Data.Traversable         (for, traverse)
-
+import qualified Data.Vector              as V
+import           System.Random.MWC
 
 data Tree a
     = Nil
     | Node (Tree a) a (Tree a)
-    deriving (Show)
+    deriving (Show, Eq)
 
 {- walk a tree in ascending order-}
 walk :: forall a. Eq a => Tree a -> Chan (Maybe Int) -> IO ()
@@ -27,3 +30,46 @@ same t1 t2 = undefined
 
 main :: IO ()
 main = undefined
+
+walkPure :: forall a. Tree a -> [a]
+walkPure = foldr (:) []
+--walkPure Nil          = []
+--walkPure (Node l a r) = walkPure l <> [a] <> walkPure r
+
+instance Foldable Tree where
+    foldMap _ Nil          = mempty
+    foldMap f (Node l a r) = foldMap f l <> f a <> foldMap f r
+
+--instance Eq Tree where
+
+
+samePure :: forall a. Eq a => Tree a -> Tree a -> Bool
+samePure t1 t2 = walkPure t1 == walkPure t2
+
+
+shuffle :: (Ord a) => [Int] -> [a] -> [a]
+shuffle rnds xs = map snd $ sort $ zip rnds xs
+
+insert :: forall a. a -> Tree a -> Tree a
+insert = undefined
+
+size :: Int
+size = 10
+
+newTree' :: [Int] -> Int -> Tree Int
+newTree' rnds x = foldl (flip insert) Nil $ shuffle rnds $ map (x*) [1..size]
+
+newTree :: Int -> IO (Tree Int)
+newTree a = do
+    rnds <- withSystemRandom . asGenIO $ \gen -> uniformVector gen size
+    pure $ newTree' (V.toList rnds) a
+
+mainPure :: IO ()
+mainPure = do
+  tree1 <- newTree 1
+  for_ (walkPure tree1) $ \x -> putStr (show x ++ ",")
+  putStrLn ""
+  tree1' <- newTree 1
+  tree2 <- newTree 2
+  print $ samePure tree1 tree1'
+  print $ samePure tree1 tree2
