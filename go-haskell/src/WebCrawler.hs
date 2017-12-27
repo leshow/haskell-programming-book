bee6f0e0aec3ecbdfe6a65d1e927ec755fbeab8c
Evan Cameron <evan@vectorface.com>
2017-12-27 14:04:44 -0500

    par version

--- go-haskell/src/WebCrawler.hs
+++ go-haskell/src/WebCrawler.hs
@@ -9,7 +9,7 @@ import           Control.Concurrent.STM   (TQueue, TVar, atomically,
                                            modifyTVar', newTQueue, newTVar,
                                            readTQueue, readTVar, writeTQueue)
 import           Control.DeepSeq          (deepseq)
-import           Control.Monad            (forM, forM_, forever, when)
+import           Control.Monad            (forM, forM_, forever, unless, when)
 import           Data.Map.Strict          (Map)
 import qualified Data.Map.Strict          as Map
 import           Data.Monoid              ((<>))
@@ -36,15 +36,39 @@ crawlNaive url depth f
                 putStrLn $ "found: " <> url <> " \"" <> body <> "\""
                 forM_ urls $ \u -> crawlNaive u (depth - 1) f
 
-crawl :: Fetchable f => String -> Int -> f -> IO ()
-crawl url depth f
-    | depth == 0 = pure ()
-    | otherwise = undefined
+crawl :: Fetchable f => String -> Int -> f -> TQueue String -> TVar (Set String) -> IO ()
+crawl url depth f out cache
+    | depth <= 0 = pure ()
+    | otherwise = do
+        new <- atomically $ do
+            set <- readTVar cache
+            let here = Set.member url set
+            unless here $ modifyTVar' cache (Set.insert url)
+            pure here
+        unless new $ do
+            res <- fetch f url
+            case res of
+                Error s -> atomically $ writeTQueue out s
+                Ok {..} -> do
+                    atomically $ writeTQueue out ("found: " <> url <> " \"" <> body <> "\"")
+                    forConcurrently_ urls $
+                        \u -> crawl u (depth - 1) f out cache
 
 main :: IO ()
 main = do
-    c <- atomically newTQueue :: IO (TQueue String)
-    pure ()
+    out <- atomically newTQueue :: IO (TQueue String)
+    cache <- atomically $ newTVar Set.empty
+    reader <- async $ readAndPrint out
+    crawl "http://golang.org/" 4 fetcher out cache
+    threadDelay $ 10^6
+    cancel reader
+
+readAndPrint :: TQueue String -> IO ()
+readAndPrint out = forever $ do
+    s <- atomically (readTQueue out)
+    putStrLn s
+
+
 
 mainSerial :: IO ()
 mainSerial = crawlNaive "http://golang.org/" 4 fetcher
