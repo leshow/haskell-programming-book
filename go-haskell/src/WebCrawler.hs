244eb3620ffc2504aaa097816878a44c571420c9
Evan Cameron <cameron.evan@gmail.com>
2017-12-26 19:02:02 -0500

    naive crawl

--- go-haskell/src/WebCrawler.hs
+++ go-haskell/src/WebCrawler.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module WebCrawler where
 
 import           Control.Concurrent       (threadDelay)
@@ -10,6 +12,7 @@ import           Control.DeepSeq          (deepseq)
 import           Control.Monad            (forM, forM_, forever, when)
 import           Data.Map.Strict          (Map)
 import qualified Data.Map.Strict          as Map
+import           Data.Monoid              ((<>))
 import           Data.Set                 (Set)
 import qualified Data.Set                 as Set
 
@@ -25,4 +28,11 @@ class Fetchable a where
 crawlNaive :: Fetchable f => String -> Int -> f -> IO ()
 crawlNaive url depth f
     | depth == 0 = pure ()
-    | otherwise  = undefined
+    | otherwise  = do
+        res <- fetch f url
+        case res of
+            Error s -> putStrLn s
+            Ok {..} -> do
+                putStrLn $ "found: " <> url <> " \"" <> body <> "\""
+                forM_ urls $ \url -> do
+                    crawlNaive url (depth - 1) f
