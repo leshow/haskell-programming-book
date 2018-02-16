09902e682c4882b30519503df6f4db20376bbffa
Evan Cameron <evan@vectorface.com>
2018-02-16 15:53:16 -0500

    typelevel nats $ fns w/ fundeps

--- type-level/src/FunDeps.hs
+++ type-level/src/FunDeps.hs
@@ -24,6 +24,7 @@ class AnyTrue list t | list -> t
 instance AnyTrue Nil False
 instance AnyTrue (Cons True xs) True
 instance (AnyTrue list t) => AnyTrue (Cons False list) t
+
 -- type family Append a b where
 --     Append Nil xs = xs
 --     Append (Cons x xs) ys = Append xs (Cons x ys)
@@ -41,6 +42,47 @@ instance Not True False
 data Z
 data S a
 
+type N0 = Z
+type N1 = S N0
+type N2 = S N1
+type N3 = S N2
+type N4 = S N3
+type N5 = S N4
+type N6 = S N5
+type N7 = S N6
+type N8 = S N7
+
+class PEqual a b t | a b -> t
+instance PEqual Z Z True
+instance PEqual Z (S b) False
+instance PEqual (S a) Z False
+instance (PEqual a b t) => PEqual (S a) (S b) t
+
+class LT a b t | a b -> t
+instance LT Z Z False
+instance LT Z (S b) True
+instance LT (S a) Z False
+instance (LT a b t) => LT (S a) (S b) t
+
+class Range n xs | n -> xs
+instance Range Z Nil
+instance (Range n xs) => Range (S n) (Cons n xs)
+
 class AddC m n r | m n -> r
-instance AddC Z m n n
+instance AddC Z n n
 instance AddC m n r => AddC (S m) n (S r)
+
+
+class LegalCompare t | -> t
+    where legalCompare :: t
+
+instance (PEqual (S Z) (S Z) t) => LegalCompare t
+-- compare that 1 == 1
+-- stack ghci
+-- :l src/FunDeps.hs
+-- :type legalCompare
+--  legalCompare :: True
+
+-- type level $
+class Apply f a r | f a -> r
+
