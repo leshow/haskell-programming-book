2d8920d9cc6c2ee6a81457f859b4dd5235563569
Evan Cameron <evan@vectorface.com>
2018-03-22 14:51:38 -0400

    simple quicksort

--- go-haskell/src/Main.hs
+++ go-haskell/src/Main.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE RankNTypes    #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
 module Main where
 
 import           Control.Concurrent       (threadDelay)
 import           Control.Concurrent.Async (async)
 import           Data.Foldable            (for_)
+import           Data.Monoid              ((<>))
 
 say :: String -> IO ()
 say s = for_ [0..4] $ \_ -> do
@@ -13,3 +17,12 @@ main :: IO ()
 main = do
     async $ say "world"
     say "hello"
+
+-- qs :: ∀ a. Ord a ⇒ [a] → [a]
+qs :: forall a. Ord a => [a] -> [a]
+qs [] = []
+qs (pivot:xs) =
+    let left    = qs [y | y <- xs, y <=  pivot]
+        right   = qs [y | y <- xs, y > pivot]
+    in
+        left <> [pivot] <> right
